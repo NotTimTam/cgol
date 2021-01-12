7d3f06e57db41baace8c568a20227a39beb2d086
Tim Phillips <timmyiscool8@gmail.com>
2021-01-12 15:16:25 -0700

    Added color mode.

--- script.js
+++ script.js
@@ -24,8 +24,8 @@ function display(array, cellSize=5) {
     for (let row = 0; row < array.length; row++) {
         for (let column = 0; column < array[row].length; column++) {
             let color;
-            if (game[row][column] == 1) {
-                color = "black";
+            if (game[row][column].value == true) {
+                color = game[row][column].color;
             } else {
                 color = "white";
             }
@@ -47,9 +47,12 @@ function createGame(width, height, oddsOfLife=15) {
             let randNum = Math.ceil(Math.random() * 100);
 
             if (randNum <= oddsOfLife) {
-                newRow.push(true);
+                let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+                color = colors[Math.floor(Math.random() * colors.length)];
+                newRow.push({ value: true, color: color });
+
             } else {
-                newRow.push(false);
+                newRow.push({ value: false, color: "white" });
             }
         }
         gameExport.push(newRow);
@@ -107,22 +110,22 @@ function getLivingNeighbors(x, y) {
     try {
         neighbors.push(game[y+1][x+1]);
     } catch {
-        
+        console.log("error");
     }
     try {
         neighbors.push(game[y+1][x-1]);
     } catch {
-
+        console.log("error");
     }
     try {
         neighbors.push(game[y-1][x+1]);
     } catch {
-
+        console.log("error");
     }
     try {
         neighbors.push(game[y-1][x-1]);
     } catch {
-
+        console.log("error");
     }
 
     // Diagonal neighbors via looping. Work in progress.
@@ -166,10 +169,9 @@ function getLivingNeighbors(x, y) {
     //     neighbors.push(game[y-1][x-1]);
     // }
     
-
     // Return the amount of them that are alive.
     for (let i = 0; i < neighbors.length; i++) {
-        if (neighbors[i] == true) {
+        if (neighbors[i] != undefined && neighbors[i].value == true) {
             livingNeighbors ++;
         }
     }
@@ -177,34 +179,111 @@ function getLivingNeighbors(x, y) {
     return livingNeighbors;
 }
 
+function getNeighborColors(x, y) {
+        // Find the eight neighbors adjacent to a given cell.
+        let livingNeighbors = 0;
+        let neighbors = [];
+        let neighborColors = [];
+        
+        // Horizontal and vertical neighbors.
+    
+        // IMPLEMENT: wrap around the screen from left to right and top to bottom. ---------------------------------------------- HEY OVER HERE -------------------------------------------------------------------------------------------- ||||||||||||||||||||||||||||||
+        
+        // Horizontal and vertical neighbors.
+    
+        if (y+1 >= game.length) {
+            neighbors.push(game[0][x]);
+        } else {
+            neighbors.push(game[y+1][x]);
+        }
+    
+        if (y-1 < 0) {
+            neighbors.push(game[game.length-1][x]);
+        } else {
+            neighbors.push(game[y-1][x]);
+        }
+    
+        if (x+1 >= game[y].length) {
+            neighbors.push(game[y][0]);
+        } else {
+            neighbors.push(game[y][x+1]);
+        }
+    
+        if (x-1 < 0) {
+            neighbors.push(game[y][game[y].length-1]);
+        } else {
+            neighbors.push(game[y][x-1]);
+        }
+        
+        // Diagonal neighbors.
+        try {
+            neighbors.push(game[y+1][x+1]);
+        } catch {
+        }
+        try {
+            neighbors.push(game[y+1][x-1]);
+        } catch {
+        }
+        try {
+            neighbors.push(game[y-1][x+1]);
+        } catch {
+        }
+        try {
+            neighbors.push(game[y-1][x-1]);
+        } catch {
+        }
+        
+        // Return the amount of them that are alive.
+        for (let i = 0; i < neighbors.length; i++) {
+            if (neighbors[i] != undefined && neighbors[i].value == true) {
+                neighborColors.push(neighbors[i].color);
+            }
+        }
+    
+        return mode(neighborColors);
+}
+
+function mode(arr){
+    return arr.sort((a,b) =>
+          arr.filter(v => v===a).length
+        - arr.filter(v => v===b).length
+    ).pop();
+}
+
+
 function setStatus(x, y) {
     // Get the given cells living neighbor count.
     let livingNeighbors = getLivingNeighbors(x, y);
+    let mostColor = getNeighborColors(x, y);
 
     // Get the given cells current state.
-    let cellStatus = game[y][x];
+    let cellStatus = game[y][x].value;
 
     // Run through the rules and see which apply.
     if (cellStatus == true) {
         // If the cell is alive.
         if (livingNeighbors < 2) {
             // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-            game[y][x] = false;
+            game[y][x].value = false;
+            game[y][x].color = "white";
             return;
         } else if (livingNeighbors == 2 || livingNeighbors == 3) {
             // Any live cell with two or three live neighbours lives on to the next generation.
-            game[y][x] = true;
+            game[y][x].value = true;
+            game[y][x].color = mostColor;
             return;
         } else if (livingNeighbors > 3) {
             // Any live cell with more than three live neighbours dies, as if by overpopulation.
-            game[y][x] = false;
+            game[y][x].value = false;
+            game[y][x].color = "white";
             return;
         }
     } else {
         // If the cell is dead.
         if (livingNeighbors == 3) {
             // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-            game[y][x] = true;
+            game[y][x].value = true;
+            game[y][x].color = mostColor;
             return;
         }
     }
